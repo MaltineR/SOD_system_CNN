8db68fdb17c0a5e1609930ca87ff67d8f05159d8
MaltineR <maltina.rama@gmail.com>
2026-05-05 21:39:39 +0200

    Add encoder-decoder for SOD model

---
+++ model.py
@@ -0,0 +1,42 @@
+import torch
+import torch.nn as nn
+
+
+class SODModel(nn.Module):
+    def __init__(self):
+        super(SODModel, self).__init__()
+
+        self.enc1 = nn.Sequential(
+            nn.Conv2d(3, 16, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(2)
+        )
+
+        self.enc2 = nn.Sequential(
+            nn.Conv2d(16, 32, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(2)
+        )
+
+        self.enc3 = nn.Sequential(
+            nn.Conv2d(32, 64, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(2)
+        )
+
+        self.dec1 = nn.Sequential(
+            nn.ConvTranspose2d(64, 32, kernel_size=2, stride=2),
+            nn.ReLU()
+        )
+
+        self.dec2 = nn.Sequential(
+            nn.ConvTranspose2d(32, 16, kernel_size=2, stride=2),
+            nn.ReLU()
+        )
+
+        self.dec3 = nn.Sequential(
+            nn.ConvTranspose2d(16, 8, kernel_size=2, stride=2),
+            nn.ReLU()
+        )
+
+        self.out = nn.Conv2d(8, 1, kernel_size=1)
